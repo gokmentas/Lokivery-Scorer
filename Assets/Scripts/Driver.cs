879f646a84c6f337bcca87c2b2da88a130bb5501
gokmentas <56593104+gokmentas@users.noreply.github.com>
2025-03-10 23:19:46 +0300

    Time.deltaTime
    Made the movement, frame rate independent.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 150f;
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,10 +14,10 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal");
-        float moveAmount = Input.GetAxis("Vertical");
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        transform.Rotate(0, 0, -steerAmount * steerSpeed);
-        transform.Translate(0, moveAmount * moveSpeed, 0);
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
