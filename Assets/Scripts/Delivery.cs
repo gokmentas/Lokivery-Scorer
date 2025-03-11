71a26fd211447685678b0f8a528329a72737f030
gokmentas <56593104+gokmentas@users.noreply.github.com>
2025-03-12 00:18:00 +0300

    Changing color
    Changing the color of the player after It picked up a package and turning it's original color when the package is delivered.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,7 +3,17 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+
     [SerializeField] float destroyDelay = 0f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,12 +25,18 @@ public class Delivery : MonoBehaviour
         if(collision.CompareTag("Package") && !hasPackage)
         {
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, destroyDelay);
+
             Debug.Log("Package picked up!");
-        }else if (collision.CompareTag("Customer") && hasPackage)
+        }
+        else if (collision.CompareTag("Customer") && hasPackage)
         {
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+
             Debug.Log("Delivered package.");
+
         }
     }
 }
